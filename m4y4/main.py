7bc966277f53e1d93360098b037b642f8315299a
maximus4000 <144921384+maximus4000@users.noreply.github.com>
2023-12-29 11:48:20 +0700

    Update main.py

--- m4y4/main.py
+++ m4y4/main.py
@@ -1,14 +1,44 @@
 #Импорт
 from flask import Flask, render_template,request, redirect
-
+#Подключение библиотеки баз данных
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+#Подключение SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#Создание db
+db = SQLAlchemy(app)
+#Создание таблицы
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(50), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    def __repr__(self):
+        return f'<User {self.id}>'
 
 #Запуск страницы с контентом
 @app.route('/#home')
 def index():
     return render_template('index.html')
+
+@app.route('/', methods=['GET','POST'])
+def form_create():
+    if request.method == 'POST':
+        email =  request.form['email']
+        text =  request.form['text']
+
+        #Создание объкта для передачи в дб
+
+        card = User(email=email, text=text)
+
+        db.session.add(card)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('index.html')
 
 
 #Динамичные скиллы
